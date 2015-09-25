b1bf31e0ecdc543500f8d0f78e185b28c48b933d
bdavies01 <bd18216@pausd.us>
2015-09-24 18:42:16 -0700

    Stuff

--- src/Unit.java
+++ src/Unit.java
@@ -1,9 +1,13 @@
+import java.awt.Graphics;
 
 public class Unit {
 	protected int x, y;
 	public Unit(int xCoord, int yCoord) {
 		this.x = xCoord;
 		this.y = yCoord;
+		
+	}
+	public void paint(Graphics g){
 		
 	}
 }
