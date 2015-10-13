c1d78a56e3b6b5352aacb5ed486aec7df26db993
bdavies01 <bd18216@pausd.us>
2015-10-12 23:11:27 -0700

    KeyListener stuff

--- src/Board.java
+++ src/Board.java
@@ -1,34 +1,15 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 
-public class Board extends JFrame implements KeyListener{
+public class Board extends JFrame{
 	private static final long serialVersionUID = 1L;
+	public KeyPress k = new KeyPress();
 	public Board() {
 		Grid g = new Grid();
 		this.add(g, BorderLayout.CENTER);
 		setSize(785, 807);
 		setBackground(Color.GRAY);
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 }
--- src/Entity.java
+++ src/Entity.java
@@ -3,9 +3,6 @@ abstract class Entity extends Tile{
 	public Entity(int x, int y) {
 		super(x, y);
 	}
-	public void move() {
-		
-	}
 	public void isLegit() {
 		
 	}
